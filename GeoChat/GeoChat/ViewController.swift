030a7a53775796019682671c16586c5dc69f659a
Joseph <gmremcorat@gmail.com>
2019-08-27 03:47:33 -0400

    Initial commit

---
+++ GeoChat/GeoChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoChat
+//
+//  Created by Joseph Storer on 8/27/19.
+//  Copyright © 2019 Joseph Storer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
