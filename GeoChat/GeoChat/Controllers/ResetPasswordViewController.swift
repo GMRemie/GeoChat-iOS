6ea4d378472686fcdc78ee34ad53a42852cc2876
Joseph <gmremcorat@gmail.com>
2019-09-21 22:18:10 -0400

    ROC Updates

--- GeoChat/GeoChat/Controllers/ResetPasswordViewController.swift
+++ GeoChat/GeoChat/Controllers/ResetPasswordViewController.swift
@@ -26,7 +26,9 @@ class ResetPasswordViewController: UIViewController {
     @IBAction func resetPasswordClick(_ sender: UIButton) {
         Auth.auth().sendPasswordReset(withEmail: emailAddress.text!)
         let alert = UIAlertController(title: "Password reset", message: "If a user for that email address is found, we have sent a password recovery code.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Continue", style: .default))
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { (UIAlertAction) in
+            self.dismiss(animated: true)
+        }))
         self.present(alert, animated: true)
     }
 }
